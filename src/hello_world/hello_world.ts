9d9d89a96141ff7d8ee0861b5f1ad03cab0cc34c
qinghai <qinghai@gmail.com>
2017-03-31 09:16:44 +0800

    extrenal deps

--- src/hello_world/hello_world.ts
+++ src/hello_world/hello_world.ts
@@ -1,7 +1,10 @@
+import {VERSION} from "lodash";
+
 export const bye = () => {
   return 'See ya!';
 };
 
 export function greet(name:string="qinghai"){
-  return  `Hello World! ${name}`;
+  return  `Hello World! ${name} lodash: ${VERSION}`;
+ // return  `Hello World! ${name} lodash: ${VERSION}`;
 }
